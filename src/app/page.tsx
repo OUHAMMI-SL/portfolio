6cb7b1feb6c31d025887edf2d2904be7cfef9616
OUHAMMI-SL <mohamed.ouhammi@scenius-lab.com>
2024-05-19 22:27:20 +0100

    made layers of projects

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,113 +1,104 @@
-import Image from "next/image";
+import HomePage from "@/compenents/Home/Home";
+import programer from "@/assets/programer.jpg"
+import Image from 'next/image';
+import CodeIcon from '@mui/icons-material/Code';
+import IconButton from '@mui/material/IconButton';
+import LinkedInIcon from '@mui/icons-material/LinkedIn';
+import FacebookIcon from '@mui/icons-material/Facebook';
+import GitHubIcon from '@mui/icons-material/GitHub';
+import XIcon from '@mui/icons-material/X';
+import LocalPostOfficeIcon from '@mui/icons-material/LocalPostOffice';
+import WavingHandIcon from '@mui/icons-material/WavingHand';
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:size-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <div className="h-screen bg-black border-white items-center ">
+    <div className="flex justify-around h-32 items-center w-full">
+        <div className="flex flex-row  justify-around space-x-3 items-center">
+            <div className="flex  flex-col items-center ">
+                <CodeIcon className="text-yellow-400 " style={{width:60, height:60}}/>
+            </div>
+            <div className="flex  flex-col items-center">
+                <span className="text-lg font-semibold text-white">OUHAMMI MOHAMED</span>
+                <span className="text-lg font-semibold text-yellow-400">Web Developer</span>
+            </div>
         </div>
-      </div>
+        <div className="flex justify-center text-white font-bold space-x-10 text-lg ">
+            <span className="hover:text-yellow-500">Intro</span>
+            <span className="hover:text-yellow-500">Why me</span>
+            <span className="hover:text-yellow-500">Technologies</span>
+            <span className="hover:text-yellow-500">Experience</span>
+            <span className="hover:text-yellow-500">Contact</span>
+        </div>
 
-      <div className="relative z-[-1] flex place-items-center before:absolute before:h-[300px] before:w-full before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 sm:before:w-[480px] sm:after:w-[240px] before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
+    </div>
 
-      <div className="mb-32 grid text-center lg:mb-0 lg:w-full lg:max-w-5xl lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
 
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
+    {/* HOme page  */}
 
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-balance text-sm opacity-50">
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+    <div className='w-full h-[70%] flex flex-center justify-center items-center space-x-10'>
+          <div className='flex flex-center w-[40%] w h-full items-center text-white'>
+          <div className="flex flex-col justify-between items-center space-x-2">
+                  <div className="flex flex-center justify-center space-x-2 w-full">
+                  <div className="flex  flex-col">
+                    <span className="text-lg font-semibold text-white">I{"'"}am</span><br></br>
+                      <span className="text-5xl font-bold text-yellow-400 border-b-4 border-white ">OUHAMMI  MOHAMED {" "}
+                      <span className="text-center mb-10"><WavingHandIcon className="text-yellow-500 " style={{width:60, height:60}}/></span>
+                    </span><br></br>
+                    <span className="text-5xl font-bold text-white animate-left-to-right"> Full Stack Developer</span>
+                    <h1 className="text-[24px] sm:text-[26px] md:text-[33px] xl:text-[48px] leading-[36px] lg:leading-[43px] xl:leading-[50px] 2xl:leading-[60px] font-semibold leading-snug mb-7">
+                        Intégrez la première
+                        {/* Text with underline animation */}
+                        <div className="relative inline-block">
+                            <span
+                                className={`ml-1.5 md:ml-3 absolute z-10 bg-main left-0 bottom-[11px] sm:bottom-[10px] md:bottom-[15px] lg:bottom-[12px] xl:bottom-[15px] 2xl:bottom-[20px]  h-[8px] sm:h-[12px] lg:h-[15px] `}>
+                            </span>
+                            <span className="relative z-20">&nbsp; communauté &nbsp;</span>
+                        </div>
+                        sélective
+                        <div className="relative inline-block">
+                            <span
+                                className={`ml-3 md:ml-4 z-10 bg-main left-0 bottom-[11px] sm:bottom-[10px] md:bottom-[15px] lg:bottom-[12px] xl:bottom-[15px] 2xl:bottom-[20px] absolute h-[8px] sm:h-[12px] lg:h-[15px] `}>
+                            </span>
+                            <span className="relative z-20">&nbsp; de formateurs indépendants &nbsp;</span>
+                        </div>
+                    </h1>
+                 </div>
+                  
+                  </div>
+                  <div className=" mt-10 w-full">
+                    <IconButton color="primary" aria-label="LinkedIn" >
+                      <LinkedInIcon style={{width:50, height:50}}/>
+                      </IconButton>
+                      <IconButton color="primary" aria-label="Facebook">
+                        <LocalPostOfficeIcon style={{width:50, height:50}}/>
+                      </IconButton>
+                      <IconButton color="primary" aria-label="GitHub">
+                        <GitHubIcon style={{width:50, height:50}}/>
+                      </IconButton>
+                      <IconButton color="primary" aria-label="Close">
+                        <XIcon style={{width:50, height:50}}/>
+                   </IconButton>
+                    <div className=" flex flex-row justify-between space-x-3">
+                          <button className="w-full py-3 border-2 rounded-full hover:bg-yellow-400 hover:text-black">Hire me</button>
+                          <button className="text-white border-yellow-400 w-full py-3 rounded-full border-2 hover:bg-white text-yellow-400">  Downlead CV</button>
+                      </div>
+                  </div>
+                {/* <p className="text-justify font-bold w-[80%]">
+                    I have over 6.5 years of commercial experience as a Front End Developer (including Full Stack Node.js knowledge)
+                    and 6.5 years of academic learning experience at a local technological university.
+                    I hold a Master s degree in Software Engineering from ZPS University
+                    of Ukraine and have an Upper Intermediate level of proficiency in written and spoken English.
+                    I am highly motivated, detail-oriented, and a responsible employee who is always interested in building mutually-beneficial partnerships.
+                    Have any questions or offers? Feel free to contact me on social networks or by email 24/7.
+                </p> */}
+                </div>
+                
+          </div>
+          <div className='flex-flex-center w-[40%] w h-[80%] items-center '>
+            <img src={programer.src} alt="" className="w-full h-full rounded-full"  />
+          </div>
+    </div>
+</div>
   );
 }
---
+++ src/compenents/Home/Home.js
@@ -0,0 +1,50 @@
+import CodeIcon from '@mui/icons-material/Code';
+import programer from "@/assets/programer.jpg"
+import Image from 'next/image';
+export default function Home(){
+    return (
+        // <div className="h-screen bg-black border-white ">
+        //         <div className="flex justify-around h-40 items-center w-full">
+        //             <div className="flex flex-row  justify-around space-x-3 items-center">
+        //                 <div className="flex  flex-col items-center ">
+        //                     <CodeIcon className="text-yellow-400 " style={{width:60, height:60}}/>
+        //                 </div>
+        //                 <div className="flex  flex-col items-center">
+        //                     <span className="text-lg font-semibold text-white">OUHAMMI MOHAMED</span>
+        //                     <span className="text-lg font-semibold text-yellow-400">Web Developer</span>
+        //                 </div>
+        //             </div>
+        //             <div className="flex justify-center text-white font-bold space-x-10 text-lg ">
+        //                 <span className="hover:text-yellow-500">Intro</span>
+        //                 <span className="hover:text-yellow-500">Why me</span>
+        //                 <span className="hover:text-yellow-500">Technologies</span>
+        //                 <span className="hover:text-yellow-500">Experience</span>
+        //                 <span className="hover:text-yellow-500">Contact</span>
+        //             </div>
+
+        //         </div>
+
+
+        //         {/* HOme page  */}
+
+        //         <div className='w-[80%] border-2 border-white h-[50%] flex flex-row '>
+        //               <div className=' w-1/2 border-2 h-full text-white'>
+        //                     <p>
+        //                         I have over 6.5 years of commercial experience as a Front End Developer (including Full Stack Node.js knowledge)
+        //                         and 6.5 years of academic learning experience at a local technological university.
+        //                         I hold a Master s degree in Software Engineering from ZPS University
+        //                         of Ukraine and have an Upper Intermediate level of proficiency in written and spoken English.
+        //                         I am highly motivated, detail-oriented, and a responsible employee who is always interested in building mutually-beneficial partnerships.
+        //                         Have any questions or offers? Feel free to contact me on social networks or by email 24/7.
+        //                     </p>
+        //               </div>
+        //               <div className=' w-1/2 border-2 h-full '>
+        //                 <Image src={programer.src} alt="" width={500} height={300} >
+        //                 </Image>
+        //               </div>
+        //         </div>
+        //  </div>
+        <>
+        </> 
+    );
+}
